bf9a1fcd4f9b27422b6293bd2d0f66e525f5cca9
Francisco Galera <francisco.galera@dvag.com>
2021-05-14 14:31:45 +0200

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NewListingPageComponent } from './new-listing-page/new-listing-page.com
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 import {FormsModule} from "@angular/forms";
 import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ListingDataFormComponent } from './listing-data-form/listing-data-form.
     MyListingsPageComponent,
     NewListingPageComponent,
     EditListingPageComponent,
-    ListingDataFormComponent
+    ListingDataFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
